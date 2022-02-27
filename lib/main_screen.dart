df1ee7a3748d4540a6028739284b2ed81d91db2f
Nerower <p0sirega121234@gmail.com>
2022-02-27 23:51:02 +0300

    screen: Add bottom nav bar

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -8,13 +8,68 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  late int _currentIndex;
+
+  late List<Widget> _pages;
+  late PageController _pageController;
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pages = [
+      // TODO: Add pages
+    ];
+
+    _currentIndex = 1;
+    _pageController = PageController(initialPage: _currentIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+
+  void _onItemTaped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Cocktail Me"),
       ),
-      body: const Center(child: Text("Main Screen"),),
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            // Minibar
+            icon: Icon(Icons.favorite),
+          ),
+          BottomNavigationBarItem(
+            // Search
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            // Random
+            icon: Icon(Icons.autorenew_rounded),
+          ),
+        ],
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: _onItemTaped,
+      ),
     );
   }
 }
